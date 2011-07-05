d138b1b2eb4e94b7da093be1c7123a6e1b0765df
Akira Ohnishi <s06206ao@gmail.com>
2011-07-05 18:17:02 +0900

    メッセージの改善

--- lib/check-date/hook.rb
+++ lib/check-date/hook.rb
@@ -3,7 +3,7 @@ Gem.pre_install do |installer|
   year_sec = 365 * 24 * 60 * 60
 
   if (today - installer.spec.date) / year_sec > 1 then
-    if !Gem::ConsoleUI.new.ask_yes_no("This gem was updated more than 1 years ago.\nInstall this gem?", true) then
+    if !Gem::ConsoleUI.new.ask_yes_no("This gem was updated more than 1 years ago. (last update: #{installer.spec.date})\nInstall this gem?", true) then
       abort
     end
   end
