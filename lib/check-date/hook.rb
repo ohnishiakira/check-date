00bb0610984f35dbbe3b533929a73152b8a5062b
Akira Ohnishi <s06206ao@gmail.com>
2011-07-05 17:49:15 +0900

    hookが動くようになった

--- lib/check-date/hook.rb
+++ lib/check-date/hook.rb
@@ -1,4 +1,12 @@
 Gem.pre_install do |installer|
-  puts "Way to go for installing #{installer.gem}"
+  today = Time.now
+  update = Time.utc(installer.spec.date)
+  year_sec = 365 * 24 * 60 * 60
+
+  if (today - update) / year_esc > 1 then
+    if !Gem::ConsoleUI.new.ask_yes_no("This gem was updated more than 1 years ago.\nInstall this gem?", true) then
+      abort
+    end
+  end
 end
 
