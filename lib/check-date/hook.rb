2fc38708aae1eba5973eec2d133fb67ac5452a12
Akira Ohnishi <s06206ao@gmail.com>
2011-07-27 02:40:37 +0900

    change the way to compare the date

--- lib/check-date/hook.rb
+++ lib/check-date/hook.rb
@@ -1,8 +1,7 @@
 Gem.pre_install do |installer|
-  today = Time.now
-  year_sec = 365 * 24 * 60 * 60
+  today = Date.today
 
-  if (today - installer.spec.date) / year_sec > 1 then
+  if (today - installer.spec.date).to_f > 365  then
     if !Gem::ConsoleUI.new.ask_yes_no("This gem was updated more than 1 years ago. (last update: #{installer.spec.date})\nInstall this gem?", true) then
       abort
     end
