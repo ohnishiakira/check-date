93580da6304d06d9952de503d388dcd20fafacfe
Akira Ohnishi <s06206ao@gmail.com>
2011-08-01 13:43:47 +0900

    New version 0.0.4. Refactor some code.

--- lib/check-date.rb
+++ lib/check-date.rb
@@ -1,7 +1,23 @@
 require "check-date/version"
 
-module Check
-  module Date
-    # Your code goes here...
+module CheckDate
+  def self.check_date(installer)
+    if (Date.today - last_update(installer.spec.date)).to_f > 365 then
+      ask_install(installer.spec.date)
+    end
+  end
+
+  def self.last_update(date)
+    date.class == Time ? date.to_date : date
+  end
+
+  def self.ask_install(date)
+    unless Gem::ConsoleUI.new.ask_yes_no(
+      "This gem was updated more than 1 years ago. (last update: #{date})\n" \
+      "Install this gem?",
+       true)
+    then
+      abort
+    end
   end
 end
--- lib/check-date/hook.rb
+++ lib/check-date/hook.rb
@@ -1,11 +1,5 @@
+require "check-date"
+
 Gem.pre_install do |installer|
-  today = Date.today
-  last_update = installer.spec.date.class == Time ? installer.spec.date.to_date : installer.spec.date
-
-  if (today - last_update).to_f > 365  then
-    if !Gem::ConsoleUI.new.ask_yes_no("This gem was updated more than 1 years ago. (last update: #{installer.spec.date})\nInstall this gem?", true) then
-      abort
-    end
-  end
+  CheckDate.check_date installer
 end
-
