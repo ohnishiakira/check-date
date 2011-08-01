9832d55d09e0dda9e31f6e7082c534035dc5fc68
Akira Ohnishi <s06206ao@gmail.com>
2011-08-01 13:08:51 +0900

    Fix Issue #2

--- lib/check-date/hook.rb
+++ lib/check-date/hook.rb
@@ -1,7 +1,8 @@
 Gem.pre_install do |installer|
   today = Date.today
+  last_update = installer.spec.date.class == Time ? installer.spec.date.to_date : installer.spec.date
 
-  if (today - installer.spec.date).to_f > 365  then
+  if (today - last_update).to_f > 365  then
     if !Gem::ConsoleUI.new.ask_yes_no("This gem was updated more than 1 years ago. (last update: #{installer.spec.date})\nInstall this gem?", true) then
       abort
     end
